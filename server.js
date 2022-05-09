196af5dee6b05ce39a6e6d0e74aa4b7a047c81ce
Mateusz Czekaj <mateusz.czek332@gmail.com>
2022-05-09 10:29:53 +0200

    registration system
    Dodawanie nowych użytkowników, sprawdzanie zgodności haseł, sprawdzanie czy dany użytkownik już istnieje

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-var express = require("express")
-const fs = require("fs");
+const express = require("express")
 const Datastore = require('nedb')
-var app = express()
+const app = express()
 const PORT = 3000;
 
-app.use(express.text())
+const users = new Datastore({
+    filename: 'users.db',
+    autoload: true
+});
 
+app.use(express.text())
+app.post("/addUser", function (req, res) {
+
+    let data = JSON.parse(req.body)
+
+    users.find({ userName: data.userName}, function (err, docs) {
+        if(docs.length > 0 ) // spprawdzenie czy istnieje taki urzytkownik jezeli nie dodanie go do bazy 
+            res.send({status: false})
+        else{
+            users.insert(data, function (err, newDoc) {
+                //console.log(newDoc)
+            });
+            res.send({status: true})  
+        }
+    });
+
+})
 
 app.use(express.static('static'))
 
