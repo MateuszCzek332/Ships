914c9abe278fe93785bf93b62d450fdac48169bb
Mateusz Czekaj <mateusz.czek332@gmail.com>
2022-05-13 06:55:50 +0200

    Generowanie planszy
    Generowanie planszy gracza, podstawowy hover - podświetlanie jednego pola na raz, niezależnie od wybranego statku.

--- static/js/Game.js
+++ static/js/Game.js
@@ -22,7 +22,10 @@ class Game {
 
     init = () => {
         this.createShipsToSet()
+        this.createMyBoard()
+
         this.selected = null // akualnie wybrany statek, wartosc pocztkowa null
+        this.highlighted = [] // akualnie podswietlane pola
 
         document.onclick = (event) => {
 
@@ -35,6 +38,24 @@ class Game {
             if(intersects.length>0){
                 this.selectShip(intersects[0].object.parent)
             }
+        }
+
+        document.onmousemove = (event) => {
+
+            this.mouseVector.x = (event.clientX / window.innerWidth) * 2 - 1;
+            this.mouseVector.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+            this.raycaster.setFromCamera(this.mouseVector, this.camera);
+
+            const intersects = this.raycaster.intersectObjects(this.myBoard.children);
+
+            if(intersects.length == 0)
+                this.unhiglight()
+            else if(this.selected != null){
+                if(this.highlighted.length == 0 || intersects[0].object.position.x != this.highlighted[0].position.x || intersects[0].object.position.z != this.highlighted[0].position.z )
+                    this.higlight(intersects[0].object)
+            }
+
         }
 
     }
@@ -51,6 +72,39 @@ class Game {
     unselectShip = () => {
         for(let i = 0; i< this.selected.children.length; i++)
             this.selected.children[i].material.color = {r:0, g:0, b:255}
+        this.selected = null
+    }
+
+    higlight = (field) => {
+        console.log("AAA")
+        this.unhiglight()
+        field.material.color = {r:255, g:0, b:0}
+        this.highlighted.push(field)
+    }
+
+    unhiglight = () => {
+        for(let i = 0; i<this.highlighted.length; i++){
+            this.highlighted[i].material.color = {r:0, g:85, b:255}
+        }
+        this.highlighted.length = 0;
+    }
+
+    createMyBoard = () => {
+        this.myBoard = new THREE.Object3D();
+        this.myFields = [];
+        for(let i = 0; i < 10; i++){
+            this.myFields[i] = []
+            for(let j = 0; j<10; j++){
+                let field =  new MyField()
+                field.getCube().position.set(i*field.a, 0, j*field.a)
+                this.myFields[i][j] = field.getCube()
+                this.myBoard.add(this.myFields[i][j])
+            }
+        }
+        this.myBoard.position.x = -150
+        this.myBoard.position.z = -200
+        //this.myBoard.position.set()
+        this.scene.add(this.myBoard)
     }
 
     createShipsToSet = () => {
